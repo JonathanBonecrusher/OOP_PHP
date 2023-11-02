02608c88a213b270fbbe9cc4269a3715635acfd5
JonathanB <114912415+JonathanBonecrusher@users.noreply.github.com>
2023-11-02 13:25:32 +0500

    Add files via upload

---
+++ task_12/src/SegmentFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\SegmentFunction;
+use App\Point;
+use App\Segment;
+
+function reverse(Segment $segment){
+    $newBeginPoint = new Point($segment->endPoint->x, $segment->endPoint->y);
+    $newEndPoint = new Point($segment->beginPoint->x, $segment->beginPoint->y);
+
+    return new Segment($newBeginPoint, $newEndPoint);
+}
